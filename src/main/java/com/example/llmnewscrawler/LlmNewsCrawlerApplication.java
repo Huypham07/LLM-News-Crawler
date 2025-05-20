7cc5d12231b41e3d7f05cda51e4d058cb9e3fffb
Huypham07 <86708207+Huypham07@users.noreply.github.com>
2025-05-20 23:20:59 +0700

    init project

---
+++ src/main/java/com/example/llmnewscrawler/LlmNewsCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.llmnewscrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmNewsCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmNewsCrawlerApplication.class, args);
+    }
+
+}
